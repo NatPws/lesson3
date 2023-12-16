c368fa482d6463b7d7beaf421a761cfce804ad38
NatPws <147820398+NatPws@users.noreply.github.com>
2023-12-16 18:06:31 +0300

    Error
    попытаться решить ошибку

--- Program.cs
+++ Program.cs
@@ -2,3 +2,6 @@
 string username = Console.ReadLine();
 Console.Write("Привет, ");
 Console.Write(username);
+
+по какой-то причине не срабатывает вывод имени на русском языке (для имени Dima - > Привет, Dima/ для имени Дима -> Привет, 
+                                                                 ПОЧЕМУ?
